185aa1980c53c097222101d49df48a5def6ab0a1
fukubaka0825 <st5818129@gmail.com>
2020-07-28 14:40:22 +0900

    tf cloud test

--- sample_ec2/main.tf
+++ sample_ec2/main.tf
@@ -3,17 +3,17 @@ provider "aws" {
   version = "2.23.0"
   region  = "ap-northeast-1"
 }
-
-terraform {
-  //バージョンを固定
-  required_version = "0.12.6"
-  backend "s3" {
-    // 保存するbucketとObject keyを
-    bucket = "nari-sample-terraform-backend"
-    key    = "sample_ec2/terraform.tfstate"
-    region = "ap-northeast-1"
-  }
-}
+//
+//terraform {
+//  //バージョンを固定
+//  required_version = "0.12.6"
+//  backend "s3" {
+//    // 保存するbucketとObject keyを
+//    bucket = "nari-sample-terraform-backend"
+//    key    = "sample_ec2/terraform.tfstate"
+//    region = "ap-northeast-1"
+//  }
+//}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
