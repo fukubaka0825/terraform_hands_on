cc4d76c3d973371e78af183fe31356e86e552172
fukubaka0825 <st5818129@gmail.com>
2020-07-28 14:17:25 +0900

    tf cloud test

--- sample_ec2_use_modules/ec2_instance.tf
+++ sample_ec2_use_modules/ec2_instance.tf
@@ -4,16 +4,16 @@ provider "aws" {
   region  = "ap-northeast-1"
 }
 
-terraform {
-  //バージョンを固定
-  required_version = "0.12.6"
-  backend "s3" {
-    // 保存するbucketとObject keyを
-    bucket = "nari-sample-terraform-backend"
-    key    = "sample_ec2/terraform.tfstate"
-    region = "ap-northeast-1"
-  }
-}
+//terraform {
+//  //バージョンを固定
+//  required_version = "0.12.6"
+//  backend "s3" {
+//    // 保存するbucketとObject keyを
+//    bucket = "nari-sample-terraform-backend"
+//    key    = "sample_ec2/terraform.tfstate"
+//    region = "ap-northeast-1"
+//  }
+//}
 
 module "samole_ec2" {
   source = "../modules/ec2"
